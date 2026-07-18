5cfdc29162d4a97207b80dc0c47c9a0496599d3f
nazeer_mog <nazeermog7@gmail.com>
2026-07-18 05:10:23 +0300

    edit api file

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,12 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/specialties', [SpecialtyController::class, 'index']);
     Route::get('/doctors', [DoctorController::class, 'index']);
+
+    Route::middleware('role:doctor')->group(function () {
+        Route::get('/doctor/appointments', [AppointmentController::class, 'doctorTodayAppointments']);
+        Route::get('/doctor/appointments/all', [AppointmentController::class, 'doctorAllAppointments']);
+    });
+
     Route::get('/doctor/{id}', [DoctorController::class, 'show']);
     Route::get('/doctors/{id}', [DoctorController::class, 'show']);
 
@@ -32,8 +38,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:doctor')->group(function () {
         Route::get('/appointments/today/{doctor_id}', [AppointmentController::class, 'doctorTodayAppointments']);
         Route::get('/appointments/doctor/{doctor_id}', [AppointmentController::class, 'doctorAllAppointments']);
-        Route::get('/doctor/appointments', [AppointmentController::class, 'doctorTodayAppointments']);
-        Route::get('/doctor/appointments/all', [AppointmentController::class, 'doctorAllAppointments']);
         Route::put('/appointments/{id}', [AppointmentController::class, 'updateStatus']);
         Route::put('/appointments/{id}/status', [AppointmentController::class, 'updateStatus']);
         Route::put('/appointments/{id}/notes', [AppointmentController::class, 'updateNotes']);
